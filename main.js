171ecd281947b470d63b9857146dacef36bee118
stop-the-war-in-ukraine <100492760+stop-the-war-in-ukraine@users.noreply.github.com>
2022-02-27 00:10:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+ TEXTS = [
+ "Молчишь - соучаствуешь",
+ "Я одобряю геноцид",
+ "Расскажу своим детям как это допустил",
+ "Стыдно быть русским",
+ "Или выходи или молчи",
+ "А завтра можешь поехать ты",
+
+]
+
+
+IMAGES = [
+    "war1.jpeg",
+    "war2.jpeg",
+    "war3.jpeg",
+    "war4.jpeg",
+]
+
+
+const getRandomInt = max => Math.floor(Math.random() * max);
+
+randomText = TEXTS[getRandomInt(TEXTS.length)]
+randomImage= IMAGES[getRandomInt(IMAGES.length)]
+
+document.getElementById('text').innerText = randomText
+document.getElementById('image').src = 'images/' + randomImage
